00dc53725aaf04db6b2331dcb93365c5e2b0acc0
Ajay Paluri <128213209+Ajaypaluri@users.noreply.github.com>
2025-08-14 11:06:45 +0530

    comments

--- frontend/app/services/jobService.ts
+++ frontend/app/services/jobService.ts
@@ -1,33 +1,3 @@
-// import axios from 'axios';
-
-// const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
-
-// export const jobService = {
-//   getJobs: async (params?: Record<string, any>) => {
-//     const res = await axios.get(`${API_BASE}/jobs`, { params });
-//     return res.data;
-//   },
-
-//   getJob: async (id: number) => {
-//     const res = await axios.get(`${API_BASE}/jobs/${id}`);
-//     return res.data;
-//   },
-
-//   createJob: async (payload: any) => {
-//     const res = await axios.post(`${API_BASE}/jobs`, payload);
-//     return res.data;
-//   },
-
-//   updateJob: async (id: number, payload: any) => {
-//     const res = await axios.put(`${API_BASE}/jobs/${id}`, payload);
-//     return res.data;
-//   },
-
-//   deleteJob: async (id: number) => {
-//     const res = await axios.delete(`${API_BASE}/jobs/${id}`);
-//     return res.data;
-//   },
-// };
 
 import axios from 'axios';
 
